383d3c2e3cdc22fa0e4ed3f97d26a541e7934fbf
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-03 15:34:33 +0200

    bloquage des image et puush dans tableau les data

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
 var count = 0;
-var player1 = "";
-var player2 = "";
+var data = [];
+var position = "";
 
-player1 = prompt("entrez votre nom joueur 1 !");
-player2 = prompt("joueur 2 entrez votre nom !");
+// player1 = prompt("entrez votre nom joueur 1 !");
+// player2 = prompt("joueur 2 entrez votre nom !");
 
-$("#player1").html(player1);
-$("#player2").html(player2);
+// $("#player1").html(player1);
+// $("#player2").html(player2);
 
 
 $(".case").click(function(){
-	count++;
+	
+	if ($(this).html() === ""){
+		if(count%2 === 0){
+		position = ($(this).data('position'));
+		data.push(position);
 
-	if(count%1 == 0){
-		($(this).html("<img class='image img-responsive' width='150' height='100' src='image1.jpg'/>"));
-	}if(count%2 == 0) {
-		($(this).html("<img class='image img-responsive' width='150' height='100' src='image2.png'/>"));
-	}	
+			($(this).html("<img class='image img-responsive' width='150' height='100' src='image1.jpg'/>"));	
+	
+	} else  {
+			position = ($(this).data('position'));
+			data.push(position);
+
+			($(this).html("<img class='image img-responsive' width='150' height='100' src='image2.png'/>"));
+		
+		}
+		count++;
+	}
+	console.log(data);
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var clic = "";
+// var count = 0;
+// $('.case').click(function(event){
+// 	// console.log(event.target.id);
+// 	 clic =  $(this);
